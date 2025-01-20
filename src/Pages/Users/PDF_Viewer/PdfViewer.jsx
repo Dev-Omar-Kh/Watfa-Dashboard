8ade70add4d1ca6d187c6cc5f9104f0920afdc6f
unknown <omar.er3434@gmail.com>
2025-01-20 04:22:23 +0200

    Add User Details Page & PDF Viewer File

--- src/Pages/Users/PDF_Viewer/PdfViewer.jsx
+++ src/Pages/Users/PDF_Viewer/PdfViewer.jsx
@@ -10,10 +10,12 @@ export default function PdfViewer({fileUrl, display}) {
 
     // ====== pdf-viewer ====== //
 
-    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-        'pdfjs-dist/build/pdf.worker.min.mjs',
-        import.meta.url,
-    ).toString();
+    // pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    //     'pdfjs-dist/build/pdf.worker.min.mjs',
+    //     import.meta.url,
+    // ).toString();
+
+    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
     const [isLoading, setIsLoading] = useState(true);
     const pageNumber = 1
